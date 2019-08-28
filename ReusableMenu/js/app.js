3e6168c824398511099c74316951f15faa7529b7
michaelbrooks100 <michael.brooks@totalemotion.com>
2019-08-28 12:05:41 +0100

    Clean up javascript

--- ReusableMenu/js/app.js
+++ ReusableMenu/js/app.js
@@ -1,17 +1,4 @@
-
-// document.addEventListener("DOMContentLoaded", function (event) {
-//     const hamburger = document.querySelector('.hamburger');
-//     const navlinks = document.querySelector('.nav-links');
-//     const links = document.querySelectorAll('.nav-links li');
-//     hamburger.addEventListener('click', () => {
-//         navlinks.classList.toggle("open");
-//         links.forEach(link => {
-//             link.classList.toggle("fade");
-//         });
-//     });
-// });
-
-function toggleMenu() {
+function toggleMenuOpen() {
     const navlinks = document.querySelector('.nav-links');
     const links = document.querySelectorAll('.nav-links li');
     navlinks.classList.toggle("open");
@@ -19,20 +6,3 @@ function toggleMenu() {
         link.classList.toggle("fade");
     });
 }
-
-//   function ready(callback){
-//     // in case the document is already rendered
-//     //if (document.readyState!='loading') callback();
-//     // modern browsers
-//     //else if (document.addEventListener) document.addEventListener('DOMContentLoaded', callback);
-//     // IE <= 8
-//     document.addEventListener('onreadystatechange', function(){
-//         if (document.readyState=='complete') callback();
-//     });
-// }
-
-// ready(function(){
-//     // do something
-//     const hamburger = document.querySelector('.hamburger');
-
-// });
